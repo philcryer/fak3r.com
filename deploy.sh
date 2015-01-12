547e85ed0c32530f74513b8cb07214d1d88cf8ed
root <root@web.cryer.us>
2015-01-12 13:51:56 -0600

    updated deploy script now testing

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 set -e
+# souper simple hexo deploy script
+# * check that pwd == dir in docroot
+# * check user/group of the webserver
+dir_name=`printf '%s\n' "${PWD##*/}"`
+web_user="www-data"
+web_grp="www-data"
+doc_root="/var/www/"$dir_name
 git pull
 hexo clean
 hexo generate
-rm -rf /var/www/fak3r.com/*
-mv public/* /var/www/fak3r.com/
-chown -R www-data:www-data /var/www/fak3r.com/
+rm -rf $doc_root/*
+mv public/* $doc_root/
+chown -R www-data:www-data $doc_root
 exit 0
