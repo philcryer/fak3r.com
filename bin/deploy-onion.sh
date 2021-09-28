42f5ad3aca5953c5544a7c7650ace1c180fd1767
philcryer <phil@philcryer.com>
2021-09-28 22:44:46 +0000

    updated onion deploy script

--- bin/deploy-onion.sh
+++ bin/deploy-onion.sh
@@ -2,6 +2,16 @@
 
 #set -x
 
+dir=$(echo $(pwd) | xargs -I% basename %)
+www_dir="/var/www/html/fak3r.com/"
+
+if [[ ! "${dir}" == "fak3r.com" ]]; then
+	echo "Not in the right directory"; exit 1
+fi
+
+echo "Updating code …"
+git pull
+
 echo "Setting tor url …"
 cp config.toml-tor config.toml
 
@@ -9,10 +19,10 @@ echo "Building …"
 hugo
 
 echo "Clearing old site …"
-rm -rf /var/www/html/fak3r.com/*
+rm -rf ${www_dir}/*
 
 echo "Deploying new site …"
-mv public/* /var/www/html/fak3r.com/
+mv public/* ${www_dir}
 
 echo "Resetting url to www …"
 cp config.toml-www config.toml
