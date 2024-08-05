efed151e3882b768ee796585ae910677e0151785
philcryer <phil@philcryer.com>
2024-08-05 11:32:28 -0500

    working migration branch including migration script

---
+++ blog/src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'fak3r';
+export const SITE_DESCRIPTION = 'Everyone thinks he looks daft';
